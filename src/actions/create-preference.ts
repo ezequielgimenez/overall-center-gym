7a000c5c082e2d3c90139968e2b73705133eea47
Ezequiel Gimenez <ezequielezequiel9@gmail.com>
2026-06-08 15:45:40 -0300

    edit preference mp backs urls

--- src/actions/create-preference.ts
+++ src/actions/create-preference.ts
@@ -84,9 +84,9 @@ export async function createPreference(formData: FormData) {
       external_reference: inscripcion.id.toString(),
 
       back_urls: {
-        success: `http://overallcentergym.com/gracias`,
-        failure: `http://overallcentergym.com/error-pago`,
-        pending: `http://overallcentergym.com/pendiente`,
+        success: "https://www.overallcentergym.com/gracias",
+        failure: "https://www.overallcentergym.com/error-pago",
+        pending: "https://www.overallcentergym.com/pendiente",
       },
 
       auto_return: "approved",
