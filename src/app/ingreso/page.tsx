0229073ea917963c304212bebba4b662cba521ea
Ezequiel Gimenez <ezequielezequiel9@gmail.com>
2026-06-09 19:25:39 -0300

    edit ingreso page.tsx

--- src/app/ingreso/page.tsx
+++ src/app/ingreso/page.tsx
@@ -1,9 +1,34 @@
-import LoginComp from "../components/Login";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+import LoginComp from "../components/Login";
+import { verifySession } from "@/src/lib/session";
+
+export default async function LoginPage() {
+  const cookieStore = await cookies();
+
+  const token = cookieStore.get("session")?.value;
+
+  if (token) {
+    const session = await verifySession(token);
+
+    if (session) {
+      redirect("/clientes");
+    }
+  }
   return (
     <div>
       <LoginComp />
     </div>
   );
 }
+
+// import LoginComp from "../components/Login";
+
+// export default function LoginPage() {
+//   return (
+//     <div>
+//       <LoginComp />
+//     </div>
+//   );
+// }
